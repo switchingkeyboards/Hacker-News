f37c5bed6a9f48089e3cdcd0fced3df9147aba2d
Ivan <ivanwong@findsolutiongroup.com>
2020-10-20 08:15:41 +0800

    Check props before rendering

--- pages/index.js
+++ pages/index.js
@@ -8,26 +8,28 @@ import Grid from '@material-ui/core/Grid';
 
 export default function App({ news }) {
   return (
-    <>
-      <CustomAppBar />
-      <Container>
-        <Box>
-          <Grid container spacing={3}>
-            {news.map((newsItem, i) => {
-              return (
-                <Grid item xs={12} sm={6} md={4} lg={3} key={i}>
-                  <NewsCard {...newsItem} />
-                </Grid>
-              );
-            })}
-          </Grid>
-        </Box>
-      </Container>
-    </>
+    news && (
+      <>
+        <CustomAppBar />
+        <Container>
+          <Box>
+            <Grid container spacing={3}>
+              {news.map((newsItem, i) => {
+                return (
+                  <Grid item xs={12} sm={6} md={4} lg={3} key={i}>
+                    <NewsCard {...newsItem} />
+                  </Grid>
+                );
+              })}
+            </Grid>
+          </Box>
+        </Container>
+      </>
+    )
   );
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const { news } = await queryGraphql(`
     query {
       news {
