30534eec56256dce14e708f79179710cf63b6c12
Ivan <ivanwong@findsolutiongroup.com>
2020-10-20 12:15:58 +0800

    Use firefox

--- pages/api/scraper/initBrowser.js
+++ pages/api/scraper/initBrowser.js
@@ -3,6 +3,7 @@ import puppeteer from 'puppeteer';
 const initBrowser = async () => {
   try {
     const browser = await puppeteer.launch({
+      product: 'firefox',
       headless: true,
       args: ['--disable-setuid-sandbox', '--no-sandbox', '--disable-dev-shm-usage'],
     });
